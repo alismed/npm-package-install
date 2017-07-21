fa97abdc142a76a3d73561e36905c932e9bd1c40
Alison Souza <alisonmsouza@yahoo.com.br>
2017-07-21 11:53:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## NPM Package Install
+
+List of to install common and most used packages
+
+
+```
+$ sh ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm install -g express
+npm install -g async
+npm install -g grunt
+npm install -g grunt-cli
+npm install -g http-server
+npm install -g gulp
+npm install -g pm2
+npm install -g gulp
+npm install -g mocha
+npm install -g uglifyjs2
+npm install -g karma
+npm install -g less
+npm install -g jshint
+npm install -g webpack
+npm install -g angularjs
+
